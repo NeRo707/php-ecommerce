413234c70bfe52fe000c4f936c8222ead4cb54d6
NeRo <tazo.gorgadze.02@gmail.com>
2026-02-12 20:16:45 +0400

    ignore: deleted temporary folder

--- _private/app/app.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/controllers/AuthController.php';
-require_once __DIR__ . '/controllers/BlogsController.php';
-$auth = new AuthController();
-$blogs = new BlogsController();
